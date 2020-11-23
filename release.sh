c18bd33b50e5d11f7577116921fa45252442bf31
rajicdalibor <rajicdalibor@yahoo.com>
2020-11-23 16:08:23 +0100

    added release

---
+++ release.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -eo
+
+if [[ "${CIRCLE_BRANCH}" =~ 'master' ]]; then
+  VERSION=$(npm run verion --prefix client --silent)
+
+  echo "Configure git"
+  git config user.email "rajicdalibor@yahoo.com"
+  git config user.name "rajicdalibor"
+
+  git add client/package.json client/package-lock.json
+  git commit -m "[skip ci] release '${VERSION}'"
+  git tag ${VERSION}
+
+  git push --set-upstream origin $CIRCLE_BRANCH
+  git push origin --tags
+else
+  echo "Skip releasing"
+fi
